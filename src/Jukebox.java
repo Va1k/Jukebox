9881a6dd4802644e5c91ef774abcdb3b72faa435
Va1k <andreas@valkyr.com.au>
2013-03-31 12:55:16 +1100

    Sweeping changes to everything!
    Added metadata display, aligned things a bit better, enabled anti-aliasing.

--- src/Jukebox.java
+++ src/Jukebox.java
@@ -15,15 +15,38 @@ public class Jukebox extends Applet{
 
     public void init() {
         // Sizes the applet on init to the preferred/intended dimensions
-        resize(400,300);
+        resize(600,300);
     }
 
     public void paint(Graphics g) {
-        // Basic Image
+        // No more fuzzy text! Anti-aliasing on!
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        //Define some stuff!
+        String title = "Strangeness & Charm";
+        String artist = "Florence & The Machine";
+        String length = "5:49";
+        Font fntT = new Font("Trebuchet MS", Font.PLAIN, 22);
+        Font fntA = new Font("Trebuchet MS", Font.PLAIN, 18);
+        Font fntL = new Font("Trebuchet MS", Font.PLAIN, 16);
+
+        //Let's have a mediatracker.
         MediaTrack = new MediaTracker(this);
+
+        // Gets album art and scales it to 150x150 pixels
         img = getImage(getCodeBase(), "0.jpg");
         MediaTrack.addImage(img,0);
-        g.drawImage(img, 0, 0, this);
+        g.drawImage(img, 10, 10, 150, 150, this);
+
+        // Implement the song information
+        g.setFont(fntT);
+        g.drawString(title, 170, 50);
+        g.setFont(fntA);
+        g.drawString(artist,180,80);
+        g.setFont(fntL);
+        g.drawString(length,180,100);
     }
 
 }
